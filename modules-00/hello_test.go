60d3101879c4327703991eaddff10b6f35e95063
Michael-Maina <michaelmaina02@gmail.com>
2023-07-12 13:08:13 +0300

    Added switch..case syntax

--- modules-00/hello_test.go
+++ modules-00/hello_test.go
@@ -4,15 +4,29 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Tadej")
+		got := Hello("Tadej", "")
 		want := "Hello, Tadej"
 		
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, world"
 		
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Pello", "Spanish")
+		want := "Hola, Pello"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Julian", "French")
+		want := "Bonjour, Julian"
+
 		assertCorrectMessage(t, got, want)
 	})
 }

bdc0d7e74ebfd928bd1c3b0f5c2817e4374fad3f
Michael-Maina <michaelmaina02@gmail.com>
2024-03-19 14:25:00 +0300

    Add Greet function

---
+++ dependency_injection/di.go
@@ -0,0 +1,15 @@
+package dependency_injection
+
+import (
+	"fmt"
+	"net/http"
+	"io"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
